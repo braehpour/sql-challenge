97c65c9a3498a253c711140bb508d1bd8e35a98b
braehpour <braehpour@gmail.com>
2020-09-20 11:34:34 -0500

    adjusted create_tables to correct PKEYs

--- EmployeeSQL/create_tables.sql
+++ EmployeeSQL/create_tables.sql
@@ -1,40 +1,40 @@
 CREATE TABLE dept_manager (
+	ID int NOT NULL PRIMARY KEY,
 	dept_no varchar,
-	emp_no varchar,
-	PRIMARY KEY (dept_no)
+	emp_no varchar
 );
 
 CREATE TABLE dept_emp (
+	ID int NOT NULL PRIMARY KEY,
 	emp_no int,
-	dept_no varchar,
-	PRIMARY KEY (emp_no)
+	dept_no varchar
 );
 
 CREATE TABLE departments (
+	ID int NOT NULL PRIMARY KEY,
 	dept_no varchar,
-	dept_name varchar,
-	PRIMARY KEY (dept_no)
+	dept_name varchar
 );
 
 CREATE TABLE titles (
+	ID int NOT NULL PRIMARY KEY,
 	title_id varchar,
-	title varchar,
-	PRIMARY KEY (title_id)
+	title varchar
 );
 
 CREATE TABLE salaries (
+	ID int NOT NULL PRIMARY KEY,
 	emp_no int,
-	salary int,
-	PRIMARY KEY (emp_no)
+	salary int
 );
 
 CREATE TABLE employees (
+	ID int NOT NULL PRIMARY KEY,
 	emp_no int,
 	emp_title_id varchar,
 	birth_date date,     -- https://www.tutorialspoint.com/how-to-create-a-table-with-date-column
 	first_name varchar,
 	last_name varchar,
 	sex varchar,
-	hire_date date,
-	PRIMARY KEY (emp_no)
+	hire_date date
 );
